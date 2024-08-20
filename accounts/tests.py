254ffbbbf9df4715184f5ee2cf58f33da9efd507
Poghosyan1309 <poghosyanofcourse@gmail.com>
2024-08-20 04:46:56 +0400

    Add Docker support with Dockerfile and docker-compose.yml

---
+++ accounts/tests.py
@@ -0,0 +1,65 @@
+from django.urls import reverse
+from rest_framework.test import APITestCase
+from rest_framework import status
+from django.contrib.auth import get_user_model
+from rest_framework_simplejwt.tokens import RefreshToken
+
+User = get_user_model()
+
+
+class AuthTests(APITestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(
+            email='testuser@example.com',
+            password='testpass123'
+        )
+        self.login_url = reverse('token_obtain_pair')
+        print(self.login_url, "<"*89)
+        self.register_url = reverse('register')  # Assuming you have a register endpoint
+
+    def test_user_registration(self):
+        data = {
+            'email': 'newuser@example.com',
+            'password': 'newpass123',
+            'password2': 'newpass123'
+        }
+        response = self.client.post(self.register_url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(User.objects.count(), 2)
+        self.assertEqual(User.objects.get(email='newuser@example.com').email, 'newuser@example.com')
+
+    def test_user_login(self):
+        data = {
+            'email': self.user.email,
+            'password': 'testpass123'
+        }
+        response = self.client.post(self.login_url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIn('access', response.data)
+        self.assertIn('refresh', response.data)
+
+    def test_invalid_login(self):
+        data = {
+            'email': self.user.email,
+            'password': 'wrongpassword'
+        }
+        response = self.client.post(self.login_url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_access_protected_view_with_valid_token(self):
+        token = RefreshToken.for_user(self.user)
+        self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {token.access_token}')
+
+        # Replace 'protected_view_url' with an actual protected URL
+        protected_url = reverse('protected_view_url')
+        response = self.client.get(protected_url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_access_protected_view_with_invalid_token(self):
+        self.client.credentials(HTTP_AUTHORIZATION='Bearer invalidtoken')
+
+        # Replace 'protected_view_url' with an actual protected URL
+        protected_url = reverse('protected_view_url')
+        response = self.client.get(protected_url)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
