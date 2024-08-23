9e811b699f23ae0c6fde7e2a6c6d3eb4b3e57af3
Poghosyan1309 <poghosyanofcourse@gmail.com>
2024-08-23 11:51:03 +0400

    deleted dotenv and added version for psycopg2-binary

--- lao_auth_backend/settings.py
+++ lao_auth_backend/settings.py
@@ -13,9 +13,7 @@ import os
 from pathlib import Path
 from datetime import timedelta
 import os
-from dotenv import load_dotenv
 
-load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
