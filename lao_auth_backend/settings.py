0898b25a85e76d70d4618f13494ef385dcebd3bc
Poghosyan1309 <poghosyanofcourse@gmail.com>
2024-08-23 11:40:54 +0400

    update .env

--- lao_auth_backend/settings.py
+++ lao_auth_backend/settings.py
@@ -90,12 +90,12 @@ REST_FRAMEWORK = {
 }
 DATABASES = {
     "default": {
-        'ENGINE': os.getenv('DATABASE_ENGINE'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
         'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'),
+        'PORT': os.getenv('DB_PORT', 5432),
     }
 }
 
