6286d85b71f2036656f46a0f57530c322a576727
Poghosyan1309 <poghosyanofcourse@gmail.com>
2024-08-26 06:18:09 +0400

    Add email and name to JWT token claims with custom TokenObtain serializer

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import RegisterView
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from .views import RegisterView, TokenView
+from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
-    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('login/', TokenView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -4,10 +4,13 @@ from django.shortcuts import render
 from rest_framework import generics
 from rest_framework.permissions import AllowAny
 from .models import LaoUser
-from .serializers import LaoUserSerializer
+from .serializers import LaoUserSerializer, TokenSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
 
 class RegisterView(generics.CreateAPIView):
     queryset = LaoUser.objects.all()
     permission_classes = (AllowAny,)
     serializer_class = LaoUserSerializer
+
+class TokenView(TokenObtainPairView):
+    serializer_class = TokenSerializer
