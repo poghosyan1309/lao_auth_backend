e29442c5829cc9cfb21854361f113836339ae168
Narek Poghosyan <115469812+poghosyan1309@users.noreply.github.com>
2024-09-25 19:45:48 +0400

    added mail system configure (#16)
    Co-authored-by: Poghosyan1309 <poghosyanofcourse@gmail.com>

--- lao_auth_backend/settings.py
+++ lao_auth_backend/settings.py
@@ -109,6 +109,15 @@ DATABASES = {
     }
 }
 
+# Email settings
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = os.getenv('EMAIL_HOST')
+EMAIL_PORT = os.getenv('EMAIL_PORT')
+EMAIL_USE_SSL = False
+EMAIL_HOST_TLS = True
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
