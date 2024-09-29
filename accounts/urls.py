6636631df88ca729e3c935260fb37cef961a328d
Poghosyan1309 <poghosyanofcourse@gmail.com>
2024-09-29 23:29:59 +0400

    Add endpoint to retrieve and update user data

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import RegisterView, TokenView
+from .views import RegisterView, TokenView, UserDetailView
 from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', TokenView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('user/', UserDetailView.as_view(), name='user_detail'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -2,10 +2,13 @@ from django.shortcuts import render
 
 # Create your views here.
 from rest_framework import generics
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from .models import LaoUser
 from .serializers import LaoUserSerializer, TokenSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.views import APIView
 
 class RegisterView(generics.CreateAPIView):
     queryset = LaoUser.objects.all()
@@ -14,3 +17,27 @@ class RegisterView(generics.CreateAPIView):
 
 class TokenView(TokenObtainPairView):
     serializer_class = TokenSerializer
+
+class UserDetailView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        serializer = LaoUserSerializer(request.user)
+        user = request.user
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def put(self, request):
+        user = request.user
+        serializer = LaoUserSerializer(user, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def patch(self, request):
+        user = request.user
+        serializer = LaoUserSerializer(user, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
