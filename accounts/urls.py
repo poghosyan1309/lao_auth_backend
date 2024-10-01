23d6367c112ede2e5b3d3e8bd6687a9df6b45720
Narek Poghosyan <115469812+poghosyan1309@users.noreply.github.com>
2024-10-01 17:28:37 +0400

    Added Swagger and Redoc (#18)
    Co-authored-by: Poghosyan1309 <poghosyanofcourse@gmail.com>

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,28 @@
-from django.urls import path
+from django.urls import path, re_path
 from .views import RegisterView, TokenView
 from rest_framework_simplejwt.views import TokenRefreshView
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
 
+schema_view = get_schema_view(
+    openapi.Info(
+        title="API Documentation",
+        default_version='v1',
+        description="API Documentation",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="poghosyanofcourse@gmail.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', TokenView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+    # Swagger and redoc paths
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
--- lao_auth_backend/settings.py
+++ lao_auth_backend/settings.py
@@ -41,9 +41,14 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+
+    # third party apps
     'rest_framework',
     'rest_framework_simplejwt',
     'corsheaders',
+    'drf_yasg',
+
+    # internal apps
     'accounts',
 ]
 
